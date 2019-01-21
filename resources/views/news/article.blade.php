b6b0159270757d69603011c59ff8f54174e45fe1
wangshacong <pnndt@qq.com>
2019-01-21 16:22:55 +0800

    添加资源自动提交代码

--- resources/views/news/article.blade.php
+++ resources/views/news/article.blade.php
@@ -39,6 +39,20 @@
 </head>
 
 <body>
+        <script>
+                (function(){
+                    var bp = document.createElement('script');
+                    var curProtocol = window.location.protocol.split(':')[0];
+                    if (curProtocol === 'https') {
+                        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+                    }
+                    else {
+                        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+                    }
+                    var s = document.getElementsByTagName("script")[0];
+                    s.parentNode.insertBefore(bp, s);
+                })();
+                </script>
     <!--通用导航 start-->
     <div class="top">
         @include('gong.head')
