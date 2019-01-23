f69baa86d0f9d5c30dfdc2a33f281356d7601c6e
wangshacong <pnndt@qq.com>
2019-01-23 16:53:08 +0800

    1

--- config/database.php
+++ config/database.php
@@ -101,7 +101,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => 'zjscxnews',
+            'database' => 'zjscnews',
             'username' => 'root',
             'password' => 'jinyu2017666',
             'unix_socket' => env('DB_SOCKET', ''),
